828ee67fb971c71ea862b3d22c8cabc04ad3dfd2
Hongjin Zhu <summerellaooo@gmail.com>
2021-03-06 20:53:24 +0000

    Sum From a to b Recursively

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (fromN < toN){
+    return fromN + sum(fromN + 1, toN);
+  }else if (fromN === toN){
+    return fromN;
+  }
+};
 
 module.exports = sum;
